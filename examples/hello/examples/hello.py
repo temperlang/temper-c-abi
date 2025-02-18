0f3985ee78a2908d605185901807c4c987309217
Tom <tom@temper.systems>
2025-02-18 14:31:51 -0700

    Try wrapper
    Signed-off-by: Tom <tom@temper.systems>

--- examples/hello/examples/hello.py
+++ examples/hello/examples/hello.py
@@ -5,15 +5,42 @@ import sys as s
 
 
 def init():
+    # TODO Init the ctypes dll stuff here also?
     _lib.hello_init()
 
 
+# def greeting_for(name: str) -> str:
+#     name_string = _lib.hello_String_new(name.encode())
+#     try:
+#         return _string_value(_lib.hello_greeting_for(name_string))
+#     finally:
+#         _lib.hello_String_free(name_string)
+
+
 def greeting_for(name: str) -> str:
-    name_string = _lib.hello_String_new(name.encode())
-    try:
-        return _string_value(_lib.hello_greeting_for(name_string))
-    finally:
-        _lib.hello_String_free(name_string)
+    with String.from_str(name) as name_string:
+        with String(_lib.hello_greeting_for(name_string.value)) as greeting:
+            return str(greeting)
+
+
+class String:
+    def from_str(s: str):
+        return String(_lib.hello_String_new(s.encode()))
+
+    def __init__(self, value):
+        self.value = value
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        _lib.hello_String_free(self.value)
+
+    def __str__(self):
+        length: int = _lib.hello_String_length(self.value) + 1
+        buffer = c.create_string_buffer(length)
+        _lib.hello_String_copy(self.value, buffer, length)
+        return buffer.value.decode()
 
 
 def _string_value(hs):

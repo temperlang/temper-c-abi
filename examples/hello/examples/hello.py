1dd4178a63bd77700a22a9f1e0eeecc58aaad72a
Tom <tom@temper.systems>
2025-02-18 15:23:03 -0700

    Use weakref finalize
    Signed-off-by: Tom <tom@temper.systems>

--- examples/hello/examples/hello.py
+++ examples/hello/examples/hello.py
@@ -2,34 +2,26 @@ import contextlib as ctx
 import ctypes as c
 import pathlib as p
 import sys as s
+import weakref as w
 
 
 def greeting_for(name: str) -> str:
-    with _string_mktemp(name) as name_string:
-        return _string_to_str(_lib.hello_greeting_for(name_string))
+    return str(String(_lib.hello_greeting_for(String.from_str(name).value)))
 
 
-def _string_make(s: str):
-    return _lib.hello_String_new(s.encode())
+class String:
+    def from_str(s: str):
+        return String(_lib.hello_String_new(s.encode()))
 
+    def __init__(self, value):
+        self.value = value
+        w.finalize(self, _lib.hello_String_free, value)
 
-@ctx.contextmanager
-def _string_mktemp(s: str):
-    hs = _string_make(s)
-    try:
-        yield hs
-    finally:
-        _lib.hello_String_free(hs)
-
-
-def _string_to_str(hs):
-    try:
-        length: int = _lib.hello_String_length(hs) + 1
+    def __str__(self):
+        length: int = _lib.hello_String_length(self.value) + 1
         buffer = c.create_string_buffer(length)
-        _lib.hello_String_copy(hs, buffer, length)
+        _lib.hello_String_copy(self.value, buffer, length)
         return buffer.value.decode()
-    finally:
-        _lib.hello_String_free(hs)
 
 
 def _choose_lib_name(base: str) -> str:
